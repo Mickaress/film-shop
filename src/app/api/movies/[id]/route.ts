9d2e13c3df84435fbc498230cf649fbbdf34af68
Mickaress <kaishevmichael@gmail.com>
2023-10-05 01:14:53 +0800

    movie route

---
+++ src/app/api/movies/[id]/route.ts
@@ -0,0 +1,53 @@
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(request: Request, { params }: Props) {
+  try {
+    const { id } = params;
+
+    const response = await db.movie.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      include: {
+        genres: {
+          select: {
+            genre: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+        products: true,
+        country: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    const movie = {
+      id: response?.id,
+      title: response?.title,
+      year: response?.year,
+      country: response?.country.name,
+      description: response?.description,
+      genres: response?.genres.map((genre) => genre.genre.name),
+      products: response?.products,
+    };
+
+    return NextResponse.json(movie, { status: 200 });
+  } catch (error) {
+    throw NextResponse.json(
+      { message: 'Ошибка получения данных' },
+      { status: 500 },
+    );
+  }
+}
