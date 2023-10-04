314b166a8936a94a7c72cfd92c4863aecb227268
Mickaress <kaishevmichael@gmail.com>
2023-10-04 21:57:23 +0800

    sign up page

---
+++ src/components/forms/SignUpForm.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Button from '../ui/Button';
+import Input from '../ui/Input';
+import { SignUpType, signUpSchema } from '@/utils/zodSchemas';
+
+const SignUpForm = () => {
+  const schema = signUpSchema;
+  const form = useForm<SignUpType>({
+    resolver: zodResolver(schema),
+    defaultValues: {},
+  });
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = async (data: SignUpType) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h1 className="text-center text-3xl mb-5">Регистрация</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-2 items-center"
+        noValidate
+      >
+        <Input
+          type="text"
+          placeholder="E-mail"
+          variant="text"
+          error={errors.email?.message}
+          {...register('email')}
+        />
+        <Input
+          type="password"
+          placeholder="Пароль"
+          variant="text"
+          error={errors.password?.message}
+          {...register('password')}
+        />
+        <Input
+          type="text"
+          placeholder="ФИО"
+          variant="text"
+          error={errors.fullName?.message}
+          {...register('fullName')}
+        />
+        <Input
+          type="number"
+          placeholder="+7xxxxxxxxxx"
+          variant="text"
+          error={errors.phone?.message}
+          {...register('phone')}
+        />
+        <Input
+          type="text"
+          placeholder="Адрес"
+          variant="text"
+          error={errors.address?.message}
+          {...register('address')}
+        />
+        <Button variant="contained" type="submit">
+          Зарегистрироваться
+        </Button>
+      </form>
+      <p className="text-center mt-3">
+        Есть аккаунт?{' '}
+        <Button variant="text" href="/sign-in" className="text-blue">
+          Войдите
+        </Button>
+      </p>
+    </>
+  );
+};
+
+export default SignUpForm;
--- src/utils/zodSchemas.ts
+++ src/utils/zodSchemas.ts
@@ -10,3 +10,32 @@ export const signInSchema = z.object({
 });
 
 export type SignInType = z.infer<typeof signInSchema>;
+
+// sign up schema
+export const signUpSchema = z.object({
+  email: z.string().min(1, 'Пустое поле').email('Некорректная почта'),
+  password: z.string().min(1, 'Пустое поле').min(8, 'Минимум 8 символов'),
+  fullName: z.string().refine(
+    (value) => {
+      const fullNamePattern = /^[а-яА-ЯёЁ]+\s[а-яА-ЯёЁ]+(\s[а-яА-ЯёЁ]+)?$/;
+
+      return fullNamePattern.test(value);
+    },
+    {
+      message: 'Некорректное ФИО',
+    },
+  ),
+  phone: z.string().refine(
+    (value) => {
+      const phonePattern = /^\+7\d{10}$/;
+
+      return phonePattern.test(value);
+    },
+    {
+      message: 'Некорректный формат: +7xxxxxxxxxx',
+    },
+  ),
+  address: z.string().nonempty('Пустое поле'),
+});
+
+export type SignUpType = z.infer<typeof signUpSchema>;
