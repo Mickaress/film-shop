edf8fa89a7cb303108f276eb92176fa389c77512
Mickaress <kaishevmichael@gmail.com>
2023-10-05 16:13:23 +0800

    fix movie list item

--- src/components/MoviesListItem.tsx
+++ src/components/MoviesListItem.tsx
@@ -15,7 +15,7 @@ const MoviesListItem: FC<Props> = ({ id, title }) => {
       <Image
         src={poster}
         alt={title}
-        className="w-[150px] h-[230px] rounded-lg border-dark_gray border"
+        className="w-[150px] h-[230px] rounded-lg border-darkgray border"
       />
       <div className="absolute top-0 left-0 w-[150px] h-[230px] rounded-lg bg-black/50 opacity-0 hover:opacity-100 cursor-pointer flex items-center">
         <p className="text-white opacity-100 text-xl w-[150px] text-center">
