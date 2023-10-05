b4c06158c17fcb227619df2852f4a875cb7a9fab
Mickaress <kaishevmichael@gmail.com>
2023-10-05 16:15:38 +0800

    refactor main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,9 @@ const Home = () => {
   return (
     <main>
       <h1 className="bold text-[48px] my-5">Фильмы</h1>
-      <div className="flex gap-4">
-        <aside className="min-w-[300px] bg-white rounded-lg p-5">
-          <Sidebar />
-        </aside>
-        <div className="bg-white w-full h-full rounded-lg p-5 flex flex-col items-center">
-          <MoviesList />
-        </div>
+      <div className="flex gap-4 items-start mb-5">
+        <Sidebar />
+        <MoviesList />
       </div>
     </main>
   );
