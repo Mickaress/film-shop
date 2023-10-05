2a48506547493696eebc7eec73eb5d5b13e0ef5b
Mickaress <kaishevmichael@gmail.com>
2023-10-05 16:38:14 +0800

    sidebar refactor

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,58 +1,19 @@
-'use client';
-
-import CheckboxWithSearch from '../GenreCheckboxGroup';
-import Input from '../ui/Input';
-import { useSetQueryParams } from '@/hooks/useSetQueryParams';
-import { useGetAllGenresQuery } from '@/api/moviesApi/hooks/useGetAllGenresQuery';
-import { useGetAllCountriesQuery } from '@/api/moviesApi/hooks/useGetAllCountriesQuery';
-import GenreCheckboxGroup from '../GenreCheckboxGroup';
-import CountryCheckboxGroup from '../CountryCheckboxGroup';
+import GenreFilter from '../GenreFilter';
+import CountryFilter from '../CountryFilter';
+import YearRangeFilter from '../yearRangeFilter';
+import TitleFilter from '../TitleFilter';
 
 export default function Sidebar() {
-  const setQueryParams = useSetQueryParams();
-
-  const handleChangeTitle = (value: string) => {
-    setQueryParams({ title: value });
-  };
-
-  const handleChangeCountry = (id: number) => {
-    setQueryParams({ country: id.toString() });
-  };
-
-  const handleChangeMinYear = (year: string) => {
-    setQueryParams({ year_start: year });
-  };
-
-  const handleChangeMaxYear = (year: string) => {
-    setQueryParams({ year_end: year });
-  };
-
   return (
     <aside className="min-w-[300px] bg-white rounded-lg p-5">
       <p className="text-base bold font-bold">Название</p>
-      <Input
-        variant="text"
-        placeholder="Поиск..."
-        onChange={(event) => handleChangeTitle(event.target.value)}
-      />
+      <TitleFilter />
       <p className="text-base bold font-bold mt-5">Жанр</p>
-      <GenreCheckboxGroup />
+      <GenreFilter />
       <p className="text-base bold font-bold mt-5">Страна</p>
-      <CountryCheckboxGroup />
+      <CountryFilter />
       <p className="text-base bold font-bold mt-5">Годы</p>
-      <div className="flex justify-between">
-        <Input
-          variant="number"
-          placeholder="0"
-          onChange={(event) => handleChangeMinYear(event.target.value)}
-        />
-        <span>-</span>
-        <Input
-          variant="number"
-          placeholder="0"
-          onChange={(event) => handleChangeMaxYear(event.target.value)}
-        />
-      </div>
+      <YearRangeFilter />
     </aside>
   );
 }
