e1f0fec874f1db2f5be02af794cbee8c29cc4a34
Mickaress <kaishevmichael@gmail.com>
2023-10-05 16:38:04 +0800

    year range filter component

---
+++ src/components/YearRangeFilter.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Input from './ui/Input';
+import { useSetQueryParams } from '@/hooks/useSetQueryParams';
+
+const YearRangeFilter = () => {
+  const setQueryParams = useSetQueryParams();
+
+  const handleChangeMinYear = (year: string) => {
+    setQueryParams({ year_start: year });
+  };
+
+  const handleChangeMaxYear = (year: string) => {
+    setQueryParams({ year_end: year });
+  };
+
+  return (
+    <div className="flex justify-between">
+      <Input
+        variant="number"
+        placeholder="0"
+        onChange={(event) => handleChangeMinYear(event.target.value)}
+      />
+      <span>-</span>
+      <Input
+        variant="number"
+        placeholder="0"
+        onChange={(event) => handleChangeMaxYear(event.target.value)}
+      />
+    </div>
+  );
+};
+
+export default YearRangeFilter;
