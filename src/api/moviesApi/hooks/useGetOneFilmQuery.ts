969eca45c58f5c9cfd5d1c70f5ab9a2b4e1daa75
Mickaress <kaishevmichael@gmail.com>
2023-10-17 20:18:29 +0800

    rename

--- src/api/moviesApi/hooks/useGetOneFilmQuery.ts
+++ src/api/moviesApi/hooks/useGetOneFilmQuery.ts
@@ -2,9 +2,9 @@ import { MovieDetailType } from '@/models/Movie';
 import { useQuery } from '@tanstack/react-query';
 import { getOneMovie } from '..';
 
-export const useGetOnedMovieQuery = (id: number) => {
+export const useGetOnedMovieQuery = (movieId: number) => {
   return useQuery<MovieDetailType>({
-    queryKey: ['film', id],
-    queryFn: () => getOneMovie(id),
+    queryKey: ['film', movieId],
+    queryFn: () => getOneMovie(movieId),
   });
 };
