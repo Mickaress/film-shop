4964fd96d5de578935ae983e38869b586c128634
Mickaress <kaishevmichael@gmail.com>
2023-10-17 20:26:44 +0800

    refactor product list item

--- src/components/productList/ProductListItem.tsx
+++ src/components/productList/ProductListItem.tsx
@@ -1,20 +1,38 @@
 'use client';
 
-import { MovieProductsType } from '@/models/Movie';
 import Image from 'next/image';
 import React from 'react';
 import Button from '../ui/Button';
 import { useAddCartProductMutation } from '@/api/cartApi/hooks/useAddCartProductMutation';
+import { useQueryClient } from '@tanstack/react-query';
+import { MovieProductType } from '@/models/Movie';
 
 type ProductListItemProps = {
-  product: MovieProductsType;
+  product: MovieProductType;
+  movieId: number;
 };
 
-const ProductListItem: React.FC<ProductListItemProps> = ({ product }) => {
-  const { mutate: addCartProduct } = useAddCartProductMutation();
+const ProductListItem: React.FC<ProductListItemProps> = ({
+  product,
+  movieId,
+}) => {
+  const { mutate: addCartProduct, isLoading } = useAddCartProductMutation();
+  const queryClient = useQueryClient();
   const image = require(`@/api/static/productsImages/${product.id}.png`);
 
   const handleAddToCart = () => {
+    const prevMovieProducts = queryClient.getQueryData<MovieProductType[]>([
+      'products',
+      movieId,
+    ]);
+    const newMovieProducts = prevMovieProducts?.map((prevMovieProduct) => {
+      if (prevMovieProduct.id === product.id) {
+        return { ...prevMovieProduct, isInCart: true };
+      }
+      return { ...prevMovieProduct };
+    });
+    console.log(newMovieProducts);
+    queryClient.setQueryData(['products', movieId], newMovieProducts);
     addCartProduct(product.id);
   };
 
@@ -29,11 +47,12 @@ const ProductListItem: React.FC<ProductListItemProps> = ({ product }) => {
         {product.price} ₽
       </p>
       <Button
-        variant="contained"
+        variant="button"
         className="whitespace-nowrap text-[16px]"
         onClick={() => handleAddToCart()}
+        disabled={product.isInCart || isLoading}
       >
-        В корзину
+        {product.isInCart ? 'В корзине' : 'В корзину'}
       </Button>
     </div>
   );
