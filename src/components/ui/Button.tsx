e125c3cf41d2785c9be1faa791e287119e71459a
Mickaress <kaishevmichael@gmail.com>
2023-10-17 21:15:43 +0800

    add color prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,6 +6,7 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   href?: string;
   children: React.ReactNode;
   className?: string;
+  color?: 'blue' | 'red';
 };
 
 const Button: React.FC<Props> = ({
@@ -14,12 +15,14 @@ const Button: React.FC<Props> = ({
   children,
   className,
   disabled,
+  color = 'blue',
   ...props
 }) => {
   const classNameMapper = {
     text: 'text-blue hover:underline',
-    button:
-      'text-white py-2 px-5 bg-blue border border-blue rounded-lg hover:text-blue hover:bg-white',
+    button: `text-white py-2 px-5 border ${
+      color === 'red' ? 'bg-red border-red' : 'bg-blue border-blue'
+    } rounded-lg hover:text-blue hover:bg-white`,
   };
 
   const classNameDisabledMapper = {
