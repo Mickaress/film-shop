88d3d72749ab4d4eb60ccf926fc2e538cf27292c
Mickaress <kaishevmichael@gmail.com>
2023-10-17 20:12:20 +0800

    invalidate products on adding in cart

--- src/api/cartApi/hooks/useAddCartProductMutation.ts
+++ src/api/cartApi/hooks/useAddCartProductMutation.ts
@@ -1,12 +1,19 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import { addCartProduct } from '..';
 
 export const useAddCartProductMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (productId: number) => addCartProduct(productId),
-    onSuccess: (res) => {
-      const message = res.data.message;
+    onMutate: () => {
+      toast.loading('Добавление товара в корзину');
+    },
+    onSuccess: async (res) => {
+      await queryClient.invalidateQueries(['products']);
+      toast.dismiss();
+      const message = res.message;
       toast.success(message);
     },
     onError(error) {
